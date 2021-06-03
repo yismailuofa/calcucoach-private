63c3bfdd3fb378f855a8151d5dcab574c9f23816
Youssef Ismail <yismail@ualberta.ca>
2021-06-02 23:30:58 -0600

    Exercise Class init

--- exercise.py
+++ exercise.py
@@ -4,6 +4,8 @@ class Exercise:
 
     Attributes:
         name: str
+        numSets: int
+        numReps: int
         chestFactor: float
         shoulderFactor: float
         tricepFactor: float
@@ -11,8 +13,48 @@ class Exercise:
         legFactor: float
         backFactor: float
         cardioFactor: float
-        sport (optional): str
-        isHome: bool
-
     '''
-    pass
+    def __init__(
+        self,
+        name: str,
+        numSets: int,
+        numReps: int,
+        chestFactor: float,
+        shoulderFactor: float,
+        tricepFactor: float,
+        bicepFactor: float,
+        legFactor: float,
+        backFactor: float,
+        cardioFactor: float
+    ):
+        self.name = name
+        self.numSets = numSets
+        self.numReps = numReps
+        self.chestFactor = chestFactor
+        self.shoulderFactor = shoulderFactor
+        self.tricepFactor = tricepFactor
+        self.bicepFactor = bicepFactor
+        self.legFactor = legFactor
+        self.backFactor = backFactor
+        self.cardioFactor = cardioFactor
+
+    def __str__(self):
+        return f"{self.name}: {self.numSets} x {self.numReps}"
+
+def main():
+    a = Exercise(
+        name="Bench Press",
+        numSets=3,
+        numReps=10,
+        chestFactor=1,
+        shoulderFactor=0.2,
+        tricepFactor=0.5,
+        bicepFactor=0.6,
+        legFactor=0.1,
+        backFactor=0.2,
+        cardioFactor=0.2
+    )
+    print(a)
+
+if __name__ == "__main__":
+    main()
--- request.py
+++ request.py
@@ -4,8 +4,8 @@ generate a request based on user inputs
 1. What is your gender?
 2. What is your age? >50 do timid workouts
 3. What is your workout style?
-1. Mass: Bulking - Jacked
-    	2. Tone: Cutting - Lean - Toning
-   	 3. Athletics: Stamina - Cardio - Sports - Athlete
+    1. Mass: Bulking - Jacked
+    2. Tone: Cutting - Lean - Toning
+   	3. Athletics: Stamina - Cardio - Sports - Athlete
 4. How many days do you want to workout a week 3 , 4 , 5 ? 
 '''
