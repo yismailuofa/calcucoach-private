3fa82fea4ed9a429096ac1f66df44011749c32bd
Youssef Ismail <yismail@ualberta.ca>
2021-06-22 00:36:38 -0600

    Navbar text

---
+++ calcucoach/src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      {/* Text */}
+      {/* button to start */}
+    </div>
+  );
+}
+
+export default App;
---
+++ calcucoach/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+      <div style={{width: "100%", display: "flex", alignItems: "baseline", justifyContent: "center"}}>
+        <div style={{fontSize: "3vw", letterSpacing: "-0.06em", textAlign: "right", width: "auto"}}>contact</div>          
+        <div style={{fontSize: "7vw", letterSpacing: "-0.06em", marginInline: "7vw"}}>calcucoach</div>
+        <div style={{fontSize: "3vw", letterSpacing: "-0.06em"}}>faq</div>
+        {/* Text */}
+        {/* button to start */}
+      </div>
+    );
+  }
+  
+  export default Header;
