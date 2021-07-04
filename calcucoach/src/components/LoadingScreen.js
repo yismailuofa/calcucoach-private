dc6ff49b6e1b5ba809462bc06370782d49c5e048
Youssef Ismail <yismail@ualberta.ca>
2021-07-03 23:51:56 -0600

    loading screen

--- calcucoach/src/components/LoadingScreen.js
+++ calcucoach/src/components/LoadingScreen.js
@@ -1,21 +1,19 @@
 import { useRef, useEffect } from "react";
 import { CSSTransition } from "react-transition-group";
+import { Player } from "@lottiefiles/react-lottie-player";
 import "./styles/slideX.css";
 
-export default function LoadingScreen({
-  nextStep,
-  step
-}) {
+export default function LoadingScreen({ nextStep, step }) {
   const nodeRef = useRef(null);
 
-useEffect(() => {
+  useEffect(() => {
     if (step === 4) {
-        const time = setTimeout(() => nextStep(), 0);
-        return () => {
-            clearTimeout(time)
-        }
+      const time = setTimeout(() => nextStep(), 3000);
+      return () => {
+        clearTimeout(time);
+      };
     }
-})
+  });
 
   return (
     <CSSTransition
@@ -39,14 +37,28 @@ useEffect(() => {
           style={{
             fontSize: "4vh",
             display: "flex",
-            padding: "2vh",  
+            padding: "2vh",
             borderRadius: "4vh",
             flexDirection: "column",
             alignItems: "center",
-            background: "linear-gradient(180deg, rgba(74,134,186,1) 50%, rgba(51,96,136,1) 100%)"
+            textAlign: "center",
+            width: "80vw",
+            maxWidth: "500px",
+            background:
+              "linear-gradient(180deg, rgba(74,134,186,1) 50%, rgba(51,96,136,1) 100%)",
+            marginTop: "-10vh",
           }}
         >
-            Loading...
+          Creating your perfect workout
+          <Player
+            src="https://assets1.lottiefiles.com/packages/lf20_vhhoyvvc.json"
+            speed={0.5}
+            autoplay={true}
+            style={{
+              width: "30vh",
+              height: "30vh",
+            }}
+          />
         </div>
       </div>
     </CSSTransition>
